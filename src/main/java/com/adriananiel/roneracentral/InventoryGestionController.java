fef057c126ceb862522f53bd53540f1ff7839c12
AdrianAniel <adrianalpizarperez45@gmail.com>
2024-06-28 14:13:01 -0400

    Agregando Inventario
    trabajando con la UI del inventario y dando funcionabilidad a los botones de la interfaz, apartado inventario casi listo

--- src/main/java/com/adriananiel/roneracentral/InventoryGestionController.java
+++ src/main/java/com/adriananiel/roneracentral/InventoryGestionController.java
@@ -1,12 +1,10 @@
 package com.adriananiel.roneracentral;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,8 +19,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javax.imageio.ImageIO;
-import com.adriananiel.roneracentral.Inventario;
-import com.adriananiel.roneracentral.Ron;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 public class InventoryGestionController implements Initializable {
 
@@ -42,7 +40,7 @@ public class InventoryGestionController implements Initializable {
     private TableView<Ron> TablaView;
 
     @FXML
-    private TableColumn<Ron, Integer> CantidadView;
+    private TableColumn<Ron, String> CantidadView;
 
     @FXML
     private TableColumn<Ron, String> NombreView;
@@ -50,18 +48,50 @@ public class InventoryGestionController implements Initializable {
     @FXML
     private TableColumn<Ron, String> VencimientoView;
 
-
+    @FXML
+    private TextField NombreField;
 
     @FXML
-    void eventExaminar(MouseEvent event) {
-        FileChooser fileChooser = new FileChooser();
+    private TextField CantidadField;
+
+    @FXML
+    private TextField VencimientoField;
+
+    @FXML
+    private Button BtnActualizar;
+
+    @FXML
+    private Button BtnEliminar;
+
+    public void limpiarCampos(){
+        NombreField.clear();
+        CantidadField.clear();
+        VencimientoField.clear();
+    }
+
+    public void cargarRonInventario(){
+        inventario.cargarRonesDesdeArchivo();
+
+        ObservableList<Ron> rons = FXCollections.observableArrayList(inventario.getListaRones());
+
+        // Asigna la lista observable al TableView
+        TablaView.setItems(rons);
+
+        // Configura las columnas del TableView
+        CantidadView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCantidadEnAlmacen()).asString());
+        NombreView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getNombre()));
+        VencimientoView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFechaVencimiento()));
+    }
+
+    public void examinarPonerFoto(){FileChooser fileChooser = new FileChooser();
         // Puedes agregar más filtros si es necesario
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos de imagen", "*.png", "*.jpg")); // Filtra por archivos de texto
 
         File selectedFile = fileChooser.showOpenDialog(null); // Pasamos null ya que estamos en un contexto de aplicación sin Stage
         if (selectedFile!= null) {
             System.out.println("Archivo seleccionado: " + selectedFile.getAbsolutePath());
-            // Aquí puedes procesar el archivo seleccionado
+
+            // Aquí pone la foto seleccionada con fileChooser en la UI que establecimos
             try {
                 // Cargar la imagen utilizando ImageIO
                 BufferedImage bufferedImage = ImageIO.read(selectedFile);
@@ -73,19 +103,51 @@ public class InventoryGestionController implements Initializable {
         }
     }
 
+    @FXML
+    void eventExaminar(MouseEvent event) {
+            examinarPonerFoto();
+    }
+
+    @FXML
+    void eventAnadir(MouseEvent event) {
+        String nombre = NombreField.getText();
+        String cantidadEnAlmacen = CantidadField.getText();
+        String fechaVencimiento = VencimientoField.getText();
+
+        inventario.agregarRon(new Ron(nombre, Integer.parseInt(cantidadEnAlmacen), fechaVencimiento));
+        System.out.println("Ron Añadido");
+
+        limpiarCampos();
+
+        cargarRonInventario();
+    }
+
+    @FXML
+    private void eventActualizar(MouseEvent event) {
+        String nombre = NombreField.getText();
+        String cantidadEnAlmacen = CantidadField.getText();
+        String fechaVencimiento = VencimientoField.getText();
+
+        inventario.actualizarRon(new Ron(nombre, Integer.parseInt(cantidadEnAlmacen), fechaVencimiento));
+        System.out.println("Ron actualizado");
+
+        limpiarCampos();
+
+        cargarRonInventario();
+    }
+
+    @FXML
+    void eventEliminar(MouseEvent event) {
+        String nombre = NombreField.getText();
+        inventario.eliminarRon(nombre);
+
+        limpiarCampos();
+
+        cargarRonInventario();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        inventario.cargarRonesDesdeArchivo();
-
-        ObservableList<Ron> rons = FXCollections.observableArrayList(inventario.getListaRones());
-
-        // Asigna la lista observable al TableView
-        TablaView.setItems(rons);
-
-        // Configura las columnas del TableView
-        CantidadView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCantidadEnAlmacen()));
-        NombreView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getNombre()));
-        VencimientoView.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFechaVencimiento()));
-
+        cargarRonInventario();
     }
 }
